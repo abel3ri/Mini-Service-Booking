35de7de47074c22df1c7ee1025fff2a35bf18b71
abel3ri <abelmerete@yahoo.com>
2025-05-08 21:20:16 +0300

    chore: regsiter routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,20 +1,87 @@
 import 'package:get/get.dart';
+import 'package:mini_service_booking/app/modules/service_details/views/service_form/bindings/service_form_binding.dart';
+import 'package:mini_service_booking/app/modules/service_details/views/service_form/views/service_form_view.dart';
 
+import '../modules/auth/login/bindings/login_binding.dart';
+import '../modules/auth/login/views/login_view.dart';
+import '../modules/auth/signup/bindings/signup_binding.dart';
+import '../modules/auth/signup/views/signup_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/home_wrapper/bindings/home_wrapper_binding.dart';
+import '../modules/home_wrapper/views/home_wrapper_view.dart';
+import '../modules/on_boarding/bindings/on_boarding_binding.dart';
+import '../modules/on_boarding/views/on_boarding_view.dart';
+import '../modules/service_details/bindings/service_details_binding.dart';
+import '../modules/service_details/views/service_details_view.dart';
+import '../modules/services/bindings/services_binding.dart';
+import '../modules/services/views/services_view.dart';
+import '../modules/settings/bindings/settings_binding.dart';
+import '../modules/settings/views/settings_view.dart';
+import '../modules/splash/bindings/splash_binding.dart';
+import '../modules/splash/views/splash_view.dart';
 
 part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.HOME;
+  static const INITIAL = Routes.SPLASH;
 
   static final routes = [
     GetPage(
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.SPLASH,
+      page: () => const SplashView(),
+      binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.HOME_WRAPPER,
+      page: () => const HomeWrapperView(),
+      binding: HomeWrapperBinding(),
+      bindings: [
+        HomeBinding(),
+        SettingsBinding(),
+      ],
+    ),
+    GetPage(
+      name: _Paths.SETTINGS,
+      page: () => const SettingsView(),
+      binding: SettingsBinding(),
+    ),
+    GetPage(
+      name: _Paths.ON_BOARDING,
+      page: () => const OnBoardingView(),
+      binding: OnBoardingBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.SERVICES,
+      page: () => const ServicesView(),
+      binding: ServicesBinding(),
+    ),
+    GetPage(
+      name: _Paths.SIGNUP,
+      page: () => const SignupView(),
+      binding: SignupBinding(),
+    ),
+    GetPage(
+      name: _Paths.SERVICE_DETAILS,
+      page: () => const ServiceDetailsView(),
+      binding: ServiceDetailsBinding(),
+    ),
+    GetPage(
+      name: _Paths.SERVICE_FORM,
+      page: () => const ServiceFormView(),
+      binding: ServiceFormBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,9 +4,27 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const SPLASH = _Paths.SPLASH;
+  static const HOME_WRAPPER = _Paths.HOME_WRAPPER;
+  static const SETTINGS = _Paths.SETTINGS;
+  static const ON_BOARDING = _Paths.ON_BOARDING;
+  static const LOGIN = _Paths.LOGIN;
+  static const SERVICES = _Paths.SERVICES;
+  static const SIGNUP = _Paths.SIGNUP;
+  static const SERVICE_DETAILS = _Paths.SERVICE_DETAILS;
+  static const SERVICE_FORM = _Paths.SERVICE_FORM;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const SPLASH = '/splash';
+  static const HOME_WRAPPER = '/home-wrapper';
+  static const SETTINGS = '/settings';
+  static const ON_BOARDING = '/on-boarding';
+  static const LOGIN = '/login';
+  static const SERVICES = '/services';
+  static const SIGNUP = '/signup';
+  static const SERVICE_DETAILS = '/service-details';
+  static const SERVICE_FORM = '/service-form';
 }
